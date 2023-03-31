4df06b95f762160d89089f5edcbc8aaf493a9fb2
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-03-31 17:13:10 +0530

    dynamic feature added

--- src/components/Headeroption.jsx
+++ src/components/Headeroption.jsx
@@ -1,16 +1,20 @@
 import { Avatar } from '@mui/material';
 import "../css/header.css";
-
+import firebase from 'firebase/compat/app';
 import React from 'react'
+import { useSelector } from 'react-redux/es/exports';
+import { selectUser } from '../features/userSlice';
 
 function Headeroption({ Icon, title, avatar }) {
+
+    const user = useSelector(selectUser);
     return (
         <div className='head_opt'>
             {
                 Icon && <Icon />
             }
             {
-                avatar && <Avatar alt="Kiran Sahu" src="/static/images/avatar/Kiran.jpg]" name={avatar} />
+                avatar && <Avatar alt="Kiran Sahu" src={user.photoURL} name={avatar} onClick={e => firebase.auth().signOut()} />
             }
 
             <span>{title}</span>
