61777126e7b8f69b6ae08a7f9702074b4625efd5
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-03-31 17:10:48 +0530

    Added some redux feature firebase authentication and that's it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Widget from './components/Widget';
+import Login from './components/Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginuser, logoutuser, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
+
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(loginuser({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          photoURL: userAuth.photoURL,
+          displayName: userAuth.displayName
+        }))
+      }
+      else {
+        dispatch(logoutuser())
+      }
+    })
+  }, [])
   return (
-    <div className='app_wrapper'>
-      <Header />
-      <div className='app_body'>
-        <Sidebar />
-        <Feed />
-        <Widget />
-      </div>
-    </div>
+    <>
+      {
+        !user ? (<Login />) : (
+          <div className='app_wrapper'>
+            <Header />
+            <div className='app_body'>
+              <Sidebar />
+              <Feed />
+              <Widget />
+            </div>
+          </div>
+        )
+      }
+    </>
   );
 }
 
