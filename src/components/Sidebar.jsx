344c3af0a0665f0501aae9f944125a8007bbe371
kiransahu-dev <sahukiran98.ks@gmail.com>
2023-03-31 22:20:18 +0530

    use selector added for dynamic input

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import "../css/sidebar.css";
 import { Avatar, Icon } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 // import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 function Sidebar() {
+    const user = useSelector(selectUser)
     return (
         <div className='sidebar'>
             <div className='sidebar_prof'>
                 <img src='https://w0.peakpx.com/wallpaper/146/911/HD-wallpaper-blue-abstract-waves-creative-blue-wavy-background-blue-backgrounds-abstract-waves-waves-textures.jpg' />
 
                 <div className='prof_detail'>
-                    <Avatar />
-                    <h4>Kiran Kumar Sahu</h4>
+                    <Avatar src={user.photoURL} />
+                    <h4>{user.displayName}</h4>
                     <p>MERN Developer</p>
                 </div>
 
